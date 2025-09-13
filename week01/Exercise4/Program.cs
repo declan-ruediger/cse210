814042719f74dba6b200df5b6fce924af4c6731b
Declan Ruediger <declan.ruediger@gmail.com>
2025-09-13 19:55:30 +1000

    W1 - Exercise 4 - Complete

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numberList = new List<int>();
+
+        int numberToAdd;
+
+        // Get List of Numbers from user
+        do
+        {
+            Console.Write("Enter number: ");
+            numberToAdd = int.Parse(Console.ReadLine());
+
+            if (numberToAdd != 0)
+            {
+                numberList.Add(numberToAdd);
+            }
+        } while (numberToAdd != 0);
+
+        Console.WriteLine($"The sum is: {numberList.Sum()}");
+
+        Console.WriteLine($"The average is: {numberList.Average()}");
+
+        Console.WriteLine($"The largest number is: {numberList.Max()}");
     }
 }

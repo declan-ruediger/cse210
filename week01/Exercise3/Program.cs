609b6dca4582257895b6be6200d8d9153fabfaf8
Declan Ruediger <declan.ruediger@gmail.com>
2025-09-13 18:12:45 +1000

    W1 - Exercise 3 - Core Requirement 2 Complete

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -7,20 +7,25 @@ class Program
         Console.Write("What is the magic number? ");
         int magicNumber = int.Parse(Console.ReadLine());
 
-        Console.Write("What is your guess? ");
-        int guess = int.Parse(Console.ReadLine());
+        int guess = 0;
 
-        if (guess == magicNumber)
+        do
         {
-            Console.WriteLine("You guessed it!");
-        }
-        else if (guess > magicNumber)
-        {
-            Console.WriteLine("Lower");
-        }
-        else if (guess < magicNumber)
-        {
-            Console.WriteLine("Higher");
-        }
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+        } while (guess != magicNumber);
     }
 }

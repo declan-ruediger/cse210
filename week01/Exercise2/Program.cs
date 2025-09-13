1dcfba90ad74429ad3cfb271624216c813d70cb8
Declan Ruediger <declan.ruediger@gmail.com>
2025-09-13 17:19:48 +1000

    W1 - Exercise 2 - Complete

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letterGrade = "";
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else if (grade < 60)
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Letter Grade: {letterGrade}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Aw, shucks! You failed... You can do it next time!");
+        }
     }
 }
